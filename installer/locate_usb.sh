c230aec17c6f6411943030490f840143bc1f67b5
Josh Forester <jforest1@gmail.com>
2025-02-08 12:43:38 -0500

    Still trying to keep QLC+ away from strom stagekit pods

---
+++ installer/locate_usb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# This script will display device files (i.e. /dev/ttyUSB0) and their associated
+# descriptors (i.e. "Performance_Designed_Products_PDP_Home_Stage_Kit_003BB097")
+# while they are currently not "in use" by an application.  That is, when 
+# StageKitPied-DMXified is running, these Stage Kit devices will NOT show up in
+# the list.  When it is not running, you may use this to confirm they are
+# connected and communicating with the Raspberry Pi.
+#
+# Note:  The serial adapter currently shows even while running.
+#
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && exit
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && exit
+        echo "/dev/$devname - $ID_SERIAL"
+    )
+done
---
+++ installer/xboxStagekitPodGuard.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+LOGFILE=/var/log/stagekitpied_xboxStagekitPodGuard.log
+
+DEVICE_PATH=$1
+
+
+echo "${DEVICE_PATH}: $(/usr/bin/date) Device plugged in." >> $LOGFILE
+
+echo "${DEVICE_PATH}: Unix Permissions" >> $LOGFILE
+echo "${DEVICE_PATH}: $(ls -l "${DEVICE_PATH}")" >> $LOGFILE
+
+# Log the ACL before setting it
+echo "${DEVICE_PATH}: ACL:" >> $LOGFILE
+echo "${DEVICE_PATH}: $(getfacl "${DEVICE_PATH}")" >> $LOGFILE
+
+# Restrict permissions
+chown root:root "${DEVICE_PATH}"
+chmod 600 "${DEVICE_PATH}"
+
+# Remove ACLs
+/usr/bin/setfacl -b "${DEVICE_PATH}"
+
+echo "${DEVICE_PATH}: Permissions set and ACLs removed." >> $LOGFILE
+
+echo "${DEVICE_PATH}: Unix Permissions" >> $LOGFILE
+echo "${DEVICE_PATH}: $(ls -l "${DEVICE_PATH}")" >> $LOGFILE
+
+# Log the ACL after setting
+echo "${DEVICE_PATH}: ACL:" >> $LOGFILE
+echo "${DEVICE_PATH}: $(getfacl "${DEVICE_PATH}")" >> $LOGFILE
+
