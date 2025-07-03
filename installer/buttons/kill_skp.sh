fb3ae8e82bb4c0640420c6a8cce53db2c2329806
Josh Forester <jforest1@gmail.com>
2025-07-03 16:56:17 -0400

    Resets button state on Stagekitpied start.

--- installer/buttons/kill_skp.sh
+++ installer/buttons/kill_skp.sh
@@ -8,11 +8,18 @@
 # Set this to empty string if you do not automatically power off your DMX devices
 USE_DMX_POWER_OFF="true"
 
+# Set this to empty string if you do not have the led_drumkit software installed
+USE_LED_DRUMKIT="true"
+
+if [ -n "$USE_LED_DRUMKIT" ]; then
+    # Run the command if USE_LED_DRUMKIT is not empty
+    sudo systemctl stop leddrumkit 
+fi
+
 if [ -n "$USE_DMX_POWER_OFF" ]; then
     # Run the command if USE_DMX_POWER_OFF is not empty
     /opt/StageKitPied/power_off_dmx_devices.sh
 fi
-
 
 # Now stop QLC+
 QLC_PID=`ps -ef | grep qlcplus | grep web | tr -s ' ' | cut -f2 -d' '`
--- installer/buttons/run_skp.sh
+++ installer/buttons/run_skp.sh
@@ -6,15 +6,46 @@
 # in order to trigger the QLC+ start.
 #
 
+SKPD_PATH=/opt/StageKitPied
+
 # Set this to empty string if you do not automatically power on your DMX devices
 USE_DMX_POWER_ON="true"
 
+# Set this to empty string if you do not have the led_drumkit software installed
+USE_LED_DRUMKIT="true"
+
+# Set this to empty string if you do not use file buttons.
+USE_FILE_BUTTONS="true"
+
 if [ -n "$USE_DMX_POWER_ON" ]; then
     # Run the command if USE_DMX_POWER_ON is not empty
-    /opt/StageKitPied/power_on_dmx_devices.sh
+    $SKPD_PATH/power_on_dmx_devices.sh
 fi
 
-sudo /opt/StageKitPied/skp_wrapper.sh
+if [ -n "$USE_FILE_BUTTONS" ]; then
+	# Stop all button services to clear their state.
+	sudo systemctl stop active_touch_file_buttons
+	sudo systemctl stop switch_touch_file_buttons
+
+	# Turn all buttons "off" by removing temp files.
+	button_file_numbers=(12 19 20 25)
+	for button_file_number in "${button_file_numbers[@]}"; do
+    	if [ -f "$SKPD_PATH/button$button_file_number.tmp" ]; then
+        	rm $SKPD_PATH/button$button_file_number.tmp 
+    	fi
+	done
+
+	# Start all button services after clearing their state.
+	sudo systemctl start active_touch_file_buttons
+	sudo systemctl start switch_touch_file_buttons
+fi
+
+if [ -n "$USE_LED_DRUMKIT" ]; then
+    # Run the command if USE_LED_DRUMKIT is not empty
+    sudo systemctl start leddrumkit 
+fi
+
+sudo $SKPD_PATH/skp_wrapper.sh
 
 # Loop until the service is active
 while true; do
